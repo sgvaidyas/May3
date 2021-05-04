ee6cd82630fee58cb85c0c263d857c7f659094c5
sgvaidyas <69918605+sgvaidyas@users.noreply.github.com>
2021-05-04 10:21:41 +0530

    Add files via upload

---
+++ swapref1.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+void swap(int &a , int &b)
+{
+	int temp;
+	temp = a;
+	a = b;
+	b = temp;
+	cout<<"\n In swap a= "<<a<<" b = "<<b<<endl;
+//	cout<<"\n In swap *a= "<<*a<<" *b = "<<*b<<endl;
+}
+int main()
+{
+	int a=10,b=20;
+	cout<<"\n MAIN Before swap a= "<<a<<" b = "<<b<<endl;
+	swap(a,b);
+	cout<<"\n MAIN  After swap a= "<<a<<" b = "<<b<<endl;
+	return 0;
+}
